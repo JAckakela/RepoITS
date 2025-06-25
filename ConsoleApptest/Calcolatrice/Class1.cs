3de3830cd94165c3a905d1a5b128f559b5fd3f28
JackBossi <giacomo.bossi.14-2@accademiadigitaleliguria.it>
2025-06-25 15:34:10 +0200

    Modifiche Calcoaltrice #1

---
+++ ConsoleApptest/Calcolatrice/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcolatrice
+{
+    public class Calcolatrice
+    {
+        public Calcolatrice() { }
+
+        public double Somma(double x, double y)
+        {
+            return x + y;
+            throw new NotImplementedException("La funzione Somma non è ancora implementata.");
+        }
+    }
+}
---
+++ ConsoleApptest/TestCalcolatrice/Test1.cs
@@ -0,0 +1,21 @@
+﻿namespace TestCalcolatrice
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestSomma()
+        {
+            Calcolatrice.Calcolatrice calcolatrice = new Calcolatrice.Calcolatrice(); 
+            double risultato = calcolatrice.Somma(2, 3);
+            Assert.AreEqual(5, risultato, "La somma di 2 e 3 dovrebbe essere 5.");
+        }
+        [TestMethod]
+        public void TestSommaConZero()
+        {
+            Calcolatrice.Calcolatrice calcolatrice = new Calcolatrice.Calcolatrice();
+            double risultato = calcolatrice.Somma(0, 0);
+            Assert.AreEqual(0, risultato, "La somma di 0 e 0 dovrebbe essere 0.");
+        }
+    }
+}
